afbb7c083ff49a0445683a40274641f9a7b3c11b
Miroslav Švarc <mirasvarc1@gmail.com>
2023-03-17 09:38:28 +0100

    edit day sum pdf

--- resources/views/pdfs/DaySumPDF.blade.php
+++ resources/views/pdfs/DaySumPDF.blade.php
@@ -164,286 +164,290 @@
         <tr style="text-align: left;">
             <td><strong>Zboží:</strong></td>
             <td></td>
-            <td style="border-right:1px solid black;width:150px;"></td>
+            <td style="border-right:1px solid black;width:120px;"></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"><strong>&nbsp;&nbsp;Naloženo</strong></td>
+            <td  style="border-right:1px solid black;width:120px;"><strong>&nbsp;&nbsp;Naloženo</strong></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"><strong>&nbsp;&nbsp;Vráceno</strong></td>
+            <td  style="border-right:1px solid black;width:120px;"><strong>&nbsp;&nbsp;Vráceno</strong></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>&nbsp;&nbsp;Podpis</strong></td>
         </tr>
         @if($items['brambory_k'] > 0)
         <tr>
             <td>Brambory konzumní:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['brambory_k']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['brambory_k']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['brambory_m'] > 0)
         <tr>
             <td>Brambory malé:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['brambory_m']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['brambory_m']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['brambory_l'] > 0)
         <tr>
             <td>Brambory loupané:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['brambory_l']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['brambory_l']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['brambory_v'] > 0)
         <tr>
             <td>Brambory vařené:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['brambory_v']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['brambory_v']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['brambory_c'] > 0)
         <tr>
             <td>Brambory červené:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['brambory_c']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['brambory_c']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['cibule'] > 0)
         <tr>
             <td>Cibule:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['cibule']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['cibule']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['cesnek'] > 0)
         <tr>
             <td>Česnek:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['cesnek']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['cesnek']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['boruvky'] > 0)
         <tr>
             <td>Borůvky:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['boruvky']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['boruvky']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['cibule_25'] > 0)
         <tr>
             <td>Cibule 25kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['cibule_25'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['cibule_10'] > 0)
         <tr>
             <td>Cibule 10kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['cibule_10'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['loupane_krajene_10'] > 0)
         <tr>
             <td>Loupané krájené 10kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['loupane_krajene_10'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['loupane_krajene_5'] > 0)
         <tr>
             <td>Loupané krájené 5kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['loupane_krajene_5'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['loupane_cele_10'] > 0)
         <tr>
             <td>Loupané celé 10kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['loupane_cele_10'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['kostky_syrove_10'] > 0)
         <tr>
             <td>Kostky syrové 10kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['kostky_syrove_10'] }} Kg
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['kostky_syrove_5'] > 0)
         <tr>
             <td>Kostky syrové 5kg:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">
+            <td style="border-right:1px solid black;width:120px;">
                 {{ $items['kostky_syrove_5'] }} Kg    
             </td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['platky_syrove'] > 0)
         <tr>
             <td>Plátky syrové:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['platky_syrove']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['platky_syrove']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['hranolky_syrove'] > 0)
         <tr>
             <td>Hranolky syrové:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['hranolky_syrove']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['hranolky_syrove']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['varene_krajene'] > 0)
         <tr>
             <td>Vařené krájené:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['varene_krajene']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['varene_krajene']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['varene_kostky'] > 0)
         <tr>
             <td>Vařené kostky:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['varene_kostky']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['varene_kostky']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
         @if($items['varene_platky'] > 0)
         <tr>
             <td>Vařené plátky:</td>
             <td>&nbsp;</td>
-            <td style="border-right:1px solid black;width:150px;">{{$items['varene_platky']}} Kg</td>
+            <td style="border-right:1px solid black;width:120px;">{{$items['varene_platky']}} Kg</td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
             <td></td>
             <td></td>
             <td></td>
-            <td  style="border-right:1px solid black;width:150px;"></td>
+            <td  style="border-right:1px solid black;width:120px;"></td>
         </tr>
         @endif
 
